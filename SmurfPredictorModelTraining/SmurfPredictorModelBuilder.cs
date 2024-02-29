1df7bcee96d262735e98d0748a52e305d6dfbd0a
etca7 <122302104+etca7@users.noreply.github.com>
2024-02-29 14:38:24 +0000

    Update SmurfPredictorModelBuilder.cs

--- SmurfPredictorModelTraining/SmurfPredictorModelBuilder.cs
+++ SmurfPredictorModelTraining/SmurfPredictorModelBuilder.cs
@@ -10,7 +10,18 @@ namespace SmurfPredictorModelTraining
     {
         public SmurfPredictorModelBuilder()
         {
+        var MLDetector = new MLContext();
+        var DataLocation = Path.Combine(Environment.CurrentDirectory, "Data Collection" , "LabelledAccountsWithData.csv");
+        var DataView = mlContext.Data.LoadFromTextFile<AccountDataSchema>(DataLocation, separator: ", ");
+        var SmurfDetectorPipeline = mlContext.Transforms.Concatenate("Features", "GamesOwned", "TotalPlaytime",
+"AccountLifetime")
+        .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression());
 
+        var SmurfDetectorModel = SmurfDetectorPipeline.Fit(DataView);
+        var SmurfDetectorPredictor = SmurfDetectorModel.Transform(DataView);
+        var SmurfDetectorMetrics = mlContext.BinaryClassification.Evaluate(SmurfDetectorPredictor);
+
+        Console.WriteLine($"Accuracy: {SmurfDetectorMetrics.Accuracy:P2}");
         }
     }
 }

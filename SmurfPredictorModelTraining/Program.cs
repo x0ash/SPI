cdd398e385bab28d0c4afe6a889b67c7d279799c
gbraam <52622160+GregBraam@users.noreply.github.com>
2024-04-12 12:07:02 +0100

    Add loading of the model after training and saving. Loading and creation of prediction engine needs to be put in a class.

--- SmurfPredictorModelTraining/Program.cs
+++ SmurfPredictorModelTraining/Program.cs
@@ -1,11 +1,39 @@
-﻿namespace SmurfPredictorModelTraining
+﻿using Microsoft.ML;
+
+namespace SmurfPredictorModelTraining
 {
     internal class Program
     {
-        // Consume the labelled training data, to produce a model
         static void Main(string[] args)
         {
+            // Trains and saves model
             SmurfPredictorModelBuilder builder = new SmurfPredictorModelBuilder();
+
+            MLContext mlContext = new MLContext();
+
+            // Temporary example, belongs in its own class
+            //Load model
+            ITransformer model;
+            using (FileStream fs = new FileStream("smurfPredictorModel.zip", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                model = mlContext.Model.Load(fs, out var schema);
+            }
+
+            // Create prediction engine
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<AccountDataSchema, AccountPrediction>(model);
+
+            AccountDataSchema testAccount = new AccountDataSchema
+            {
+                GamesOwned = 2,
+                TotalPlaytime = 150,
+                AccountLifetime = 600,
+                RecentPlaytime = 0,
+            };
+
+            var prediction = predictionEngine.Predict(testAccount);
+
+            Console.WriteLine(prediction.IsSmurf);
+
         }
     }
 }
